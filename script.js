0d19b32e75d5b2c6a0f67030ed521986eff24cf8
Pulkit-Rathore <pulkitrathore770@gmail.com>
2024-09-12 19:54:40 +0530

    added middleware function to verify JsonWebToken

--- script.js
+++ script.js
@@ -28,3 +28,15 @@ const userSchema = new mongoose.Schema({
 });
 
 const user = mongoose.model("User", userSchema);
+
+// middleware to ensure the JWT
+const verifyToken  = (req, res, next) =>{
+const token = req.headers.['auth_token'];
+if(!token) return res.status(401).send("INVALID AUTHORIZATION");
+
+jwt.verify(token,'pulkits secret', (err, decoded)=>{
+    if(err) return res.status(401).send("WRONG CREDENTIAL");
+    req.user = decoded;
+    next();
+})
+}
